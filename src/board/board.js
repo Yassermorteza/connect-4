5cb6509ae95d2e1f6549c19bf0fe28ba2a430dac
yassermorteza <yasser.morteza@gmail.com>
2018-03-14 12:44:56 +0100

    only one solution and modules

--- src/board/board.js
+++ src/board/board.js
@@ -3,6 +3,7 @@
 const PIXI = require('pixi.js');
 const Howel = require('howler');
 const store = require('../store');
+const win = require('../win');
 const ai = require('../ai');
 
 const texture = PIXI.Texture.fromImage('images/board.png');
@@ -10,138 +11,208 @@ const redBall = PIXI.Texture.fromImage('images/red.png');
 const greenBall = PIXI.Texture.fromImage('images/green.png');
 
 const victorySound = new Howl({src: ['sounds/victory.mp3']});
+const loosSound = new Howl({src: ['sounds/loos.mp3']});
+const clickSound = new Howl({src: ['sounds/click.mp3']});
 
-let container = new PIXI.Container();
+const container = new PIXI.Container();
 container.hitArea = new PIXI.Rectangle(0, 0, 480, 450);
 container.position.set(350, 150);
 
-let data = {
-		    rows: [],
-		    cols: [],
-		    diagonalA: [],
-		    diagonalB: [],
-		    diagonalC: [],
-            matrix: []
-			};
-
 module.exports = {
 		
-	  	boardcell: [],
-        ball: {},
-        color: '',
-        state: '',
+  	boardcell: [],
+    matrix: [],
+    message: {},
+    ball: {},
+    color: '',
+    state: '',
+    turn: true,
 
-	    board: function(){
-		    for (let i = 0; i <= 5; i++) {
-                store.matrix[i] = [];
-		        for (let j = 0; j <= 6; j++) {
-                    store.matrix[i].push(0);
-                    store.bot.matrix[i] = [];
-                    store.user.matrix[i] = [];   
-		            store.cellIndex.push(i + '' + j);
-		            let x = j * 70;
-		            let y = i * 70;
-		            let rectangle = new PIXI.Rectangle(0, 0, 100, 100);
-		            texture.frame = rectangle;
-		            this.boardcell = new PIXI.Sprite(texture);
-		            this.boardcell.width = 70;
-		            this.boardcell.height = 70;
-		            this.boardcell.colIndex = j;
-		            this.boardcell.rowIndex = i;
-		            this.boardcell.position.set(x, y);
-		            this.boardcell.interactive = true;
-		            this.boardcell.on('click', store.event.dropBall());
-		            container.addChild(this.boardcell);
-		        }
-		    }
-		},
+    board: function(){
+	    for (let i = 0; i <= 5; i++) {
+            this.matrix[i] = [];
+	        for (let j = 0; j <= 6; j++) {
+                this.matrix[i].push(0);  
+	            store.cellIndex.push(i + '' + j);
+	            let x = j * 70;
+	            let y = i * 70;
+	            let rectangle = new PIXI.Rectangle(0, 0, 100, 100);
+	            texture.frame = rectangle;
+	            this.boardcell = new PIXI.Sprite(texture);
+	            this.boardcell.width = 70;
+	            this.boardcell.height = 70;
+	            this.boardcell.colIndex = j;
+	            this.boardcell.rowIndex = i;
+	            this.boardcell.position.set(x, y);
+	            this.boardcell.interactive = true;
+	            this.boardcell.on('click', this.dropBall());
+	            container.addChild(this.boardcell);
+	        }
+	    };
+	},
 
-		playerTurn: function(){
-            if (store.turn) {
-                this.ball = redBall;
-                this.color = 'red';
-                store.turn = false;
-            } else {
-                this.ball = greenBall;
-                this.color = 'green';
-                store.turn = true;
-            }
-            store.color = this.color;
-            store.player = new PIXI.Sprite(this.ball);
-            store.player.position.set(0, -70);
-            store.player.width = 70;
-            store.player.height = 70;
-            store.player.color = this.color;
-            container.addChild(store.player);
-        },
+	playerTurn: function(){
+        if (this.turn) {
+            this.ball = redBall;
+            this.color = 'red';
+            this.turn = false;
+        } else {
+            this.ball = greenBall;
+            this.color = 'green';
+            this.turn = true;
+        }
+        store.player = new PIXI.Sprite(this.ball);
+        store.player.position.set(0, -70);
+        store.player.width = 70;
+        store.player.height = 70;
+        store.player.color = this.color;
+        container.addChild(store.player);
+    },
 
-        showMessage: function(){
-            
-            let empty = ai.emptyCells().length;
-            if(empty < 1){
-                this.state = 'No Winner';
-            }else{
-                this.state = this.color.toUpperCase() + ' win';
-            }
+    showMessage: function(){
+        
+        let empty = this.emptyCells().length;
+        if(empty < 1){
+            this.state = 'No Winner';
+        }else{
+            this.state = this.color.toUpperCase() + ' win';
+        }
 
-            let style = new PIXI.TextStyle({
-                fontFamily: 'Arial',
-                fontSize: 36,
-                fontStyle: 'italic',
-                fontWeight: 'bold',
-                fill: ['#ffffff', '#00ff99'], // gradient
-                stroke: '#4a1850',
-                strokeThickness: 5,
-                dropShadow: true,
-                dropShadowColor: '#000000',
-                dropShadowBlur: 4,
-                dropShadowAngle: Math.PI / 6,
-                dropShadowDistance: 6,
-                wordWrap: true,
-                wordWrapWidth: 440,
-            });
+        let style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#ffffff', '#00ff99'], // gradient
+            stroke: '#4a1850',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440,
+        });
 
-            store.message = new PIXI.Text(`${this.state}!!! Play again.`, style);
-            store.message.position.set(60, 150);
-            container.addChild(store.message);
-            store.message.interactive = true;
-            store.message.cursor = "pointer";
-            store.message.on('click',this.reset());
-            store.layer.interactive = false;
-            store.onclick = false;
+        this.message = new PIXI.Text(`${this.state}!!! Play again.`, style);
+        this.message.position.set(60, 150);
+        container.addChild(this.message);
+        this.message.interactive = true;
+        this.message.cursor = "pointer";
+        this.message.on('click',this.reset());
+        store.layer.interactive = false;
+        store.onclick = false;
+        if(this.color === 'red'){    
             victorySound.play();
+        }else{
+            loosSound.play();
+        }
 
-        },
+    },
 
-        reset: function() {
+    reset: function() {
 
-        	let self =this;
+    	let self =this;
 
-        	return function(){
-        		
-	            store.onclick = true;
-	            store.turn = true;
-	            store.user = JSON.parse(JSON.stringify(data));
-	            store.bot = JSON.parse(JSON.stringify(data));
-	            store.cellIndex = [];
-	            store.countCol = 5;
-	            store.row = 0;
-	            container.removeChild(store.message);
-	            for (let i = 0; i < container.children.length; i++) {
-	                if (container.children[i].color) {
-	                    container.removeChild(container.children[i]);
-	                };
-	                container.children.forEach(item => {
-	                    if (item.color) {
-	                        container.removeChild(item);
-	                    };
-	                });
-	            }
-                store.layer.interactive = true;
-	            self.playerTurn();
-	            self.board();
-        	}
-        },
+    	return function(){
+    		
+            store.onclick = true;
+            store.cellIndex = [];
+            store.countCol = 5;
+            store.row = 0;
+            container.removeChild(self.message);
+            for (let i = 0; i < container.children.length; i++) {
+                if (container.children[i].color) {
+                    container.removeChild(container.children[i]);
+                };
+                container.children.forEach(item => {
+                    if (item.color) {
+                        container.removeChild(item);
+                    };
+                });
+            }
+            store.layer.interactive = true;
+            self.playerTurn();
+            self.board();
+    	}
+    },
 
-        container: ()=> container
+    findIndex: function (colIndex, count) {
+        let item = count + '' + colIndex;
+        if (store.countCol < 0) {
+            store.countCol = 5;
+            store.row = 0;
+            return;
+        } else {         
+            return store.cellIndex.indexOf(item);
+        };
+    },
+
+    dropBall: function (){
+        let self = this;
+        return function (){
+            let colIndex ;
+            let x ;
+            if (store.onclick) {
+                if(store.index){
+                    if(store.botCol !== ''){
+                        colIndex = store.botCol;
+                        x = colIndex * 70;
+                    }else{
+                        colIndex = Math.floor(Math.random() * Math.floor(7)); 
+                        x = colIndex * 70;
+                    }
+                }else{
+                    colIndex = this.colIndex;
+                    x = this.transform.position.x;
+                };
+
+                let i = self.findIndex(colIndex, store.countCol);
+
+                while (i < 0) {
+                    store.row++;
+                    store.countCol--;
+                    i = self.findIndex(colIndex, store.countCol);
+                }
+
+                if (~i && store.cellIndex[i]) {
+                    self.matrix[store.countCol][colIndex] = store.player.color;
+                    store.cellIndex[i] = store.player.color;
+                    store.colIndex = colIndex;
+                    store.player.position.set(x, 350 - (store.row * 70));
+                    clickSound.play();
+                    // require('../ai').checkStore();
+
+                    let empty = self.emptyCells().length;
+                    
+                    if(win.winner(self.matrix, store.player.color) || empty < 1){
+                        return self.showMessage();
+                    }
+
+                    self.playerTurn();
+                    store.row = 0;
+                    store.countCol = 5;
+                    store.index = false;
+                };
+            };
+        };
+    },
+
+    emptyCells: function() {
+        let empty = new Array();
+        for (let i = 0; i < this.matrix.length; i++) {
+            empty[i] = [];
+            for (let j = 0; j < this.matrix[i].length; j++) {
+
+                if (typeof this.matrix[i][j] === 'number') {
+                    empty[i].push(j);
+                };
+            };
+        };
+
+        return empty.filter(el => el.length > 0);
+    },
+
+    container: ()=> container
 };
--- src/win.js
+++ src/win.js
@@ -1,93 +1,81 @@
 'use strict';
 
-const store = require('./store');
-const board = require('./board/board');
-
 module.exports = {
 
     counter: 0,
 
-    sequence: function(value) {
-        let array = value.sort();
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] + 1 === array[i + 1]) {
-                this.counter++;
-                if (this.counter >= 3) {
-                    this.counter = 0;
-                    return true;
+    sequence: function(array) {
+        for (let i = 0; i < array.length; ++i) {
+            if (array[i]) {
+                for (let j = 0; j < array[i].length; ++j) {
+                    if (array[i][j] + 1 === array[i][j + 1]) {
+                        this.counter++;
+                        if (this.counter >= 3) {
+                            this.counter = 0;
+                            return true;
+                        }
+                    } else {
+                        this.counter = 0;
+                    };
                 }
-            } else {
-                this.counter = 0;
             }
-        }
+        };
 
         return false;
     },
 
-    verticalHorzintal: function(cel, row, player) {
+    diagonal: function(array) {
 
-        if (!player.rows[cel]) {
-            player.rows[cel] = [];
-            player.rows[cel].push(row);
-        } else {
-            player.rows[cel].push(row);
-            if (player.rows[cel].length >= 4) {
-                return this.sequence(player.rows[cel]);
-            };
-        };
+        for (let i = 0; i < array.length; ++i) {
 
-        if (!player.cols[row]) {
-            player.cols[row] = [];
-            player.cols[row].push(cel);
-        } else {
-            player.cols[row].push(cel);
-            if (player.cols[row].length >= 4) {
-                return this.sequence(player.cols[row]);
-            };
-        };
-
-    },
-
-    diagonal: function(cel, row, player) {
-
-        let sum = cel + row;
-        let num = Number(row + '' + cel);
-        let length = player.diagonalA.length;
-
-        if (length === 0) {
-            player.diagonalA[0] = [];
-            player.diagonalA[0].push(num);
-        } else {
-            for (let i = 0; i < length; i++) {
-                if (player.diagonalA[i]) {
-                    for (let j = 0; j < player.diagonalA[i].length; j++) {
-                        if (player.diagonalA[i][j] - 11 === num || player.diagonalA[i][j] + 11 === num) {
-                            player.diagonalA[i].push(num);
-                            if (player.diagonalA[i].length === 4) {
-                                return true;
-                            }
-                        } else {
-                            player.diagonalA[length + 1] = [];
-                            player.diagonalA[length + 1].push(num);
-                        };
+            for (let j = 0; j < array[i].length; ++j) {
+                let r = 1;
+                let c = 0;
+                let r1 = 1;
+                let c1 = 0;
+                //Southeast Northwest
+                while (j + r < 7 && i + r < 6 && array[i + c][j + c] + 1 === array[i + r][j + r]) {
+                    r++;
+                    c++;
+                    if (c >= 3) {
+                        return true
+                    };
+                };
+                // Southwest NorthEast
+                while (j - r1 >= 0 && i + r1 < 6 && array[i + c1][j - c1] - 1 === array[i + r1][j - r1]) {
+                    r1++;
+                    c1++;
+                    if (c1 >= 3) {
+                        return true
                     };
                 };
             };
         };
 
-        if (sum >= 3 && sum <= 8) {
-            if (!player.diagonalB[sum]) {
-                player.diagonalB[sum] = [];
-                player.diagonalB[sum].push(sum);
-                player.diagonalC[sum] = [];
-                player.diagonalC[sum].push(row);
-            } else {
-                player.diagonalB[sum].push(sum);
-                player.diagonalC[sum].push(row);
-                if (player.diagonalB[sum].length >= 4 && this.sequence(player.diagonalC[sum])) {
-                    return true;
+        return false;
+    },
+
+    winner: function(matrix, player) {
+        let horizontal = [];
+        let vertical = [];
+
+        for (let i = 0; i < matrix.length; i++) {
+            horizontal[i] = [];
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] === player) {
+                    horizontal[i][j] = j;
+                    if (!vertical[j]) {
+                        vertical[j] = [];
+                        vertical[j].push(i);
+                    } else {
+                        vertical[j].push(i);
+                    };
                 };
             };
         };
-    }
+
+        return this.sequence(horizontal) || this.sequence(vertical) || this.diagonal(horizontal);
+    },
+
+
 };
